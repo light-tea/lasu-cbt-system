8abd1fe27284163618cb346891e017738e471777
Light-Tea <lightistheid@gmail.com>
2026-05-21 12:31:10 +0100

    check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
 {
     if ($this->app->environment('production')) {
         URL::forceScheme('https');
+                \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+
     }
 }
 }
